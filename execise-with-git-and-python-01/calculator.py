e2717ad7f6999e5f6376b8bc9b3ec68eae33dd70
juangzj <juanjosegoyeszambrano@gmail.com>
2025-07-28 10:32:30 -0500

    Values to do operations

--- execise-with-git-and-python-01/calculator.py
+++ execise-with-git-and-python-01/calculator.py
@@ -11,3 +11,13 @@ def multiplication(a, b):
 # subtraction function
 def subtraction(a, b):
     return a - b
+
+
+# values to do a sum
+print(f"The sum is:  {sum(5, 2)}")
+
+# values to do a subtraction
+print(f"The subtraction is: {subtraction(5,2)}")
+
+# Values to do a multiplication
+print(f"The multiplication is: {multiplication(2,3)}")
